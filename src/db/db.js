efb065df4a578f383fd53cbcfa5d0a0077494225
Alex <sun123vet@gmail.com>
2022-04-16 23:14:20 +0700

    [add] deploy to production server

---
+++ src/db/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  process.env.MONGODB_URL,
+  {
+    keepAlive: true,
+    keepAliveInitialDelay: 300000,
+  },
+  function (err) {
+    if (err) return console.log("Error: ", err);
+    console.log(
+      "MongoDB Connection -- Ready state is:",
+      mongoose.connection.readyState
+    );
+  }
+);
